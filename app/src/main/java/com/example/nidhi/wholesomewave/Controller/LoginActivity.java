378f9f1e95f9e149b4cca26d0bbd01f5c266a84c
Uzair Minhas <uziminhas@gmail.com>
2017-11-06 17:32:49 -0500

    First commit

---
+++ app/src/main/java/com/example/nidhi/wholesomewave/Controller/LoginActivity.java
@@ -0,0 +1,108 @@
+package com.example.nidhi.wholesomewave.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.ProgressDialog;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.nidhi.wholesomewave.Model.User;
+import com.example.nidhi.wholesomewave.Controller.MainScreenActivity;
+import com.example.nidhi.wholesomewave.R;
+
+/**
+ * Created by nidhi on 11/3/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private ProgressDialog progressDialog;
+    private Button loginDoctor;
+    private Button loginParticipant;
+
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+        //initialize views
+        username = (EditText) findViewById(R.id.emailText);
+        password = (EditText) findViewById(R.id.pWordText);
+        progressDialog = new ProgressDialog(this);
+        //SharedPreferences mPrefs = getSharedPreferences("WaterCrowdSource", MODE_PRIVATE);
+
+        loginParticipant = (Button) findViewById(R.id.docBtn);
+        loginParticipant.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                customerActivity();
+            }
+        });
+
+
+        loginDoctor = (Button) findViewById(R.id.usrBtn);
+        loginDoctor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                changeActivity();
+            }
+        });
+    }
+
+    public void customerActivity () {
+
+        Intent intent = new Intent(this, MainScreenActivity.class);
+        //intent.putExtra("Username", username.getText().toString());
+        startActivity(intent);
+
+    }
+
+
+    public void changeActivity(){
+        Intent intent = new Intent(this, DoctorScreenActivity.class);
+        startActivity(intent);
+
+        }
+
+    public void onCancelPressed(View v) {
+        finish();
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        finish();
+    }
+
+    public void onStart() {
+        super.onStart();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
